6fd0b10b959d8884cd461b051a6eb5cbc1996e7c
muteeah9 <mutiyatsoliudeen@gmail.com>
2023-06-15 09:52:41 -0700

    4-puts.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio>
+/**
+  *main - A program that prints the size of various types on the computer it is compiled and run on
+  *Return: always 0 (success)
+  */
+int main(void)
+{
+	printf("size of a char: %d byte(s)", sizeof(char));
+	printf("size of an int: %d byte(s)", sizeof(int));
+	printf("size of a long int: %d byte(s)", sizeof(long int));
+	printf("size of a long long int: %d byte(s)", sizeof(long long int));
+	printf("Size of a float: %d byte(s)", sizeof(float));
+	return (0);
+}
