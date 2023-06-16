e79990972512cdb2bf20f7f7909c73937a6fe2fa
muteeah9 <mutiyatsoliudeen@gmail.com>
2023-06-16 16:30:07 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,13 +9,15 @@ int main(void)
 	int n;
 	int m;
 
-	for (n = 48; n <= 57; n++)
+	for (n = 48; n <= 57;)
 	{
 		putchar(n);
+		n++;
 	}
-	for (n = 97;  n <= 102; m++)
+	for (n = 97;  n <= 102;)
 	{
 		putchar(m);
+		m++;
 	}
 	putchar('\n');
 	return (0);
