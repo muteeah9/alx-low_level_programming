cc0e5e1a1e185c8c01d71e90193b2f1c82d461a5
muteeah9 <mutiyatsoliudeen@gmail.com>
2023-06-21 14:21:54 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * lowercase character, another casses, shows
+  * 0
+  *
+  *Dc: the character in ASCII mode
+  *
+  * Return: 1 for lowercase character. 0 for the rest
+  */
+int - islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+_putchar('\n');
+}
